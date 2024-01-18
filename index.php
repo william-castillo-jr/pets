56d569346ab69a6985c0f75cd43be82c65b26ade
Will Castillo <william.castillo.jr@outlook.com>
2024-01-18 12:19:34 -0500

    Created index.php controller

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+//error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require autoload file
+require_once('vendor/autoload.php');
+
+//instantiate fat-free
+$f3 = Base::instance();
+
+$f3->route('GET /', function() {
+    echo "Pet Home";
+});
+
+$f3->run();
+?>
